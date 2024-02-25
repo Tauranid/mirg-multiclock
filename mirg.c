31d643e4a0f1ad1805bf1d59c68f17450528deb5
Oskar <->
2024-02-25 22:06:14 +0100

    add divided clocks

--- mirg.c
+++ mirg.c
@@ -1,15 +1,21 @@
 #include <bcm2835.h>
 #include <stdio.h>
 #include <alsa/asoundlib.h>
+#include <stdio.h>
+#include <syslog.h>
 
-// Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
-#define PIN RPI_GPIO_P1_11
+// pins for various clock dividers
+#define PIN1 RPI_BPLUS_GPIO_J8_31
+#define PIN2 RPI_BPLUS_GPIO_J8_29
+#define PIN4 RPI_BPLUS_GPIO_J8_15
+#define PIN8 RPI_BPLUS_GPIO_J8_13
+#define PIN16 RPI_BPLUS_GPIO_J8_11
 
 // there are 12 clock ticks per 8th-note
 #define CLOCK_DIV 12
 
-// we want a 15 ms pulse
-#define PULSE_LEN 15
+// we want a 15 ms pulse but 30 seems a bit more stable
+#define PULSE_LEN 30
 
 typedef enum play_state
 {
@@ -18,20 +24,14 @@ typedef enum play_state
 }
 play_state;
 
-static void errormessage(const char *format, ...)
-{
-    va_list ap;
-    va_start(ap, format);
-    vfprintf(stderr, format, ap);
-    va_end(ap);
-    putc('\n', stderr);
-}
-
 int main(int argc, char **argv)
 {
+    openlog("slog", LOG_PID|LOG_CONS, LOG_USER);
+    syslog(LOG_EMERG, "MIRG: setting up");
+
 	if(argc != 2)
 	{
-		printf("must define hardware midi port, e.g. \"sync hw:1,0,0\"\n");
+		syslog(LOG_EMERG, "MIRG: must define hardware midi port, e.g. \"sync hw:1,0,0\"\n");
 		return 1;
 	}
 	
@@ -42,30 +42,40 @@ int main(int argc, char **argv)
 	err = snd_rawmidi_open(&midiin, NULL, portname, SND_RAWMIDI_SYNC);
 	if(err)
 	{
-		errormessage("error opening midi port\n");
+	    syslog(LOG_EMERG, "MIRG: error opening midi port\n");
 		return 1;
 	}
 	err = snd_rawmidi_read(midiin, NULL, 0);
 	if(err)
 	{
-		errormessage("error initializing midi read\n");
+		syslog(LOG_EMERG, "MIRG: error initializing midi read\n");
 		return 1;
 	}
 	
 	if (!bcm2835_init())
 	{
-		errormessage("failed to init the GPIO library\n");
+		syslog(LOG_EMERG, "MIRG: failed to init the GPIO library\n");
 		return 1;
 	}
 		
-	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN4, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN8, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN16, BCM2835_GPIO_FSEL_OUTP);
 	play_state state = play_state_stopped;
 	uint8_t ticks = 0;
-
+	uint8_t multiplier_cnt = 0;
+    syslog(LOG_EMERG, "MIRG: entering main loop");
 	while (1)
 	{
 		uint8_t status;
-		snd_rawmidi_read(midiin, &status, 1);
+		int ret;
+		if ((ret = snd_rawmidi_read(midiin, &status, 1)) < 0){
+            syslog(LOG_EMERG, "MIRG: read error: %d - %s\n", (int)ret, snd_strerror(ret));
+            syslog(LOG_EMERG, "MIRG: exiting after read error");
+            return 1;
+		}
 		
 		if(status & 0xF0) 
 		{
@@ -73,31 +83,45 @@ int main(int argc, char **argv)
 			{
 				case 0xFA: // start
 				{
+				    printf("start\n");
 					ticks = 0; // reset clock
+					multiplier_cnt = 0;
 					state = play_state_playing;
 					break;
 				}
 				case 0xFB: // continue
 				{
+			        printf("continue\n");  
+					ticks = 0; // reset clock
+					multiplier_cnt = 0;
 					state = play_state_playing;
 					break;
 				}
 				case 0xFC: // stop
 				{
+			        printf("stop\n");  
 					state = play_state_stopped;
 					break;
 				}
 				case 0xF8: // clock tick
 				{
+			        //printf("tick\n");  
 					if(ticks % CLOCK_DIV == 0) // 
 					{
 						ticks = 0; // reset clock
 						if(state == play_state_playing)
 						{
-							bcm2835_gpio_write(PIN, HIGH);
+	    			        uint32_t mask = 1 << PIN1;
+	    			        if (multiplier_cnt % 2 == 0) mask |= 1 << PIN2; 
+	    			        if (multiplier_cnt % 4 == 0) mask |= 1 << PIN4; 
+	    			        if (multiplier_cnt % 8 == 0) mask |= 1 << PIN8;
+	    			        if (multiplier_cnt % 16 == 0) mask |= 1 << PIN16; 
+	    			        
+	    			        bcm2835_gpio_set_multi(mask);
 							bcm2835_delay(PULSE_LEN);
-							bcm2835_gpio_write(PIN, LOW);
+	    			        bcm2835_gpio_clr_multi(mask);
 						}
+						multiplier_cnt++;
 					}
 					
 					if(state == play_state_playing)
